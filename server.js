6bd7e2a957cd215d65dbb512a796ee33ca760e69
Anna Eriksson <annaeriksson0516@gmail.com>
2022-04-22 11:12:08 +0200

    deploy heroku changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 const app = express()
-const PORT = 1975
+const PORT = process.env.PORT || 1975
 import hamsters from './routes/hamsters.js'
 
 import { fileURLToPath } from 'url'
